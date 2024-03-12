ebcb8784e5965d7a294e316ad62b082d7ab97df8
Luan Zekiri <luan.00@hotmail.de>
2024-03-12 15:07:26 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,94 @@
+
+const container = document.querySelector('.container');
+
+const grid = document.createElement("div");
+grid.className = "grid";
+grid.style.display = 'grid';
+grid.style.gridTemplateColumns = `repeat(16, 1fr)`;
+
+grid.style.transition = '200ms ease 50ms';
+container.appendChild(grid);
+
+
+let currentInk = "black";
+let isRainbow = false;
+
+// Add 16x16 grid
+function createGrid() {
+    let mouseClicked = false;
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++) {
+            const div = document.createElement('div');
+
+            div.addEventListener("mousedown", function () {
+                mouseClicked = true;
+                if (isRainbow) {
+                    currentInk = generateRandomColor();
+                    ink(div);
+                } else {
+                    ink(div);
+                }
+            })
+            div.addEventListener("mouseover", function () {
+                if (mouseClicked) {
+                    if (isRainbow) {
+                        currentInk = generateRandomColor();
+                        ink(div);
+                    } else {
+                        ink(div);
+                    }
+                }
+            })
+            grid.appendChild(div);
+        }
+    }
+    addEventListener("mousedown", function () {
+        mouseClicked = true;
+    });
+
+    addEventListener("mouseup", function () {
+        mouseClicked = false;
+    })
+}
+
+function ink(item) {
+    item.style.backgroundColor = currentInk;
+}
+
+function reset() {
+    container.innerHTML = "";
+    createGrid();
+}
+
+function setRainbow() {
+    isRainbow = true;
+}
+
+function setWhite() {
+    isRainbow = false;
+    currentInk = "white";
+}
+
+function setBlack() {
+    isRainbow = false;
+    currentInk = "black";
+}
+
+/*This method generates a random color in the RGB space
+ and returns it*/
+function generateRandomColor() {
+    var color1 = Math.floor(Math.random() * 255) + 1;
+    var color2 = Math.floor(Math.random() * 255) + 1;
+    var color3 = Math.floor(Math.random() * 255) + 1;
+
+    return 'rgb(' + color1
+        + ',' + color2
+        + ',' + color3 + ')';
+}
+
+
+createGrid();
+
+
+
+
